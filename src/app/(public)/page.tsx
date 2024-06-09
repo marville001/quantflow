6713f93397cb8e3aab5732bf2723988731a9ce44
Martin Mwangi <mwangimartin1904@gmail.com>
2024-06-09 18:43:04 +0300

    home page update

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,3 +1,4 @@
+import { ICONS } from "@/assets/icons";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
@@ -27,7 +28,7 @@ export default function Home() {
           <h1 className="text-grey-800 hero-large w-full md:max-w-[732px] text-center ">
             Super power for Admin Teams
           </h1>
-          < p className="text-grey-800 header-normal  w-full md:max-w-[544px] text-center mt-5 ">
+          < p className="text-grey-800 header-normal  w-full max-w-[] sm:max-w-[544px] text-center mt-5 ">
             Quantflow delivers enterprise-grade AI to automate your most tedious tasks.
           </p>
           <div className="flex items-center gap-4 mt-7">
@@ -40,7 +41,7 @@ export default function Home() {
           <Image src="/assets/images/video.png" alt="Hero" width={1120} height={600} className="w-full h-auto object-cover" />
         </div>
 
-        <div className="flex justify-between gap-5 mt-4">
+        <div className="flex justify-between gap-5 mt-4 flex-col gap-y-8 sm:flex-row">
           {
             hero_items.map((item) => (
               <div key={item.index} className="flex items-start flex-1 flex-col  gap-4">
@@ -59,8 +60,28 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="p-4">
-        
+      <div className="p-4 bg-quantflow-500">
+
+        <div className="max-w-[1120px] mx-auto py-16 lg:py-24">
+          <h2 className="hero-medium text-white">Simplify with Ai</h2>
+          <p className="body-large text-white max-w-[544px] mt-5">
+            Quantflow scales to automate administrative tasks, integrating smoothly with your existing software and workflows.
+          </p>
+
+          <div className="bg-white mt-20 p-[32px] sm:h-[400px] gap-y-6 flex-col sm:flex-row pr-0 pb-0 overflow-hidden rounded-[20px] flex">
+            <div className="w-[300px]">
+              <h3 className="text-grey-800 header-normal">Document Automation</h3>
+              <p className="body-normal mt-4 text-grey-600">
+                Automate the populating and processing of documents and web-forms, streamlining your paperwork effortlessly.
+              </p>
+              <Button variant={"outline"} className="mt-5 h-[48px] gap-2">Learn more {ICONS.arrowRightUp}</Button>
+            </div>
+            <div className="flex-1 self-end h-[360px]">
+              <Image src="/assets/images/document-automation.png" alt="Hero" width={600} height={400} className="w-full h-[300px] sm:h-full max-sm:object-left object-top object-cover" />
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   );
