ed506a58252114f8f94e186b40d8e305b08be172
Martin Mwangi <mwangimartin1904@gmail.com>
2024-06-09 20:04:40 +0300

    make header responsive

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,19 +4,34 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
 import Image from "next/image";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 interface HeaderProps { }
 
 const Header = ({ }: HeaderProps) => {
+	const [isHeaderOpen, setIsHeaderOpen] = useState(false);
+
+
 	return (
-		<div className="h-[104px] w-full flex items-center">
+		<div className="h-[104px] w-full flex items-center px-4">
 			<div className="max-w-[1280px] flex-1 mx-auto border-grey-300 border-[1px] 
 			rounded-[20px] flex justify-between gap-5 items-center p-[16px]">
 				<Link href={"/"} className="logo">
 					<Image src="/assets/images/quantflow-logo-black.png" alt="Quantflow" width={120} height={40} />
 				</Link>
-				<nav>
-					<ul className="flex items-center gap-6">
+				<nav className={cn("fixed transition-all md:static top-0 bottom-0 left-0 right-0 p-5 md:p-0 bg-white",
+					{
+						"-translate-x-0": isHeaderOpen,
+						"-translate-x-[100%] md:-translate-x-0": !isHeaderOpen
+					}
+				)}>
+					<ul className={cn("flex flex-col md:flex-row md:items-center gap-6")}
+					>
+						<span className="menu cursor-pointer self-end p-2" onClick={() => setIsHeaderOpen(!isHeaderOpen)}>
+							<X className="md:hidden" size={24} />
+						</span>
 						<li>
 							<Link href="/about" className="flex items-center text-grey-800 gap-4 cursor-pointer font-[400]">
 								<span>Platform</span>
@@ -41,13 +56,20 @@ const Header = ({ }: HeaderProps) => {
 								</svg>
 							</Link>
 						</li>
+						<li className="flex flex-col gap-4 md:hidden">
+							<Button className="w-max" variant={"outline"}>Contact us</Button>
+							<Button className="w-max">Get started</Button>
+						</li>
 					</ul>
 				</nav>
 
-				<div className="flex items-center gap-4">
+				<div className="hidden items-center gap-4 md:flex">
 					<Button variant={"outline"}>Contact us</Button>
 					<Button>Get started</Button>
 				</div>
+				<span className="menu md:hidden cursor-pointer p-2" onClick={() => setIsHeaderOpen(!isHeaderOpen)}>
+					<Menu size={24} />
+				</span>
 			</div>
 
 		</div>
